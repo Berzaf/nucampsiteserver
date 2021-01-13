fbddfc1283d3cbac8a6184116bb83b7c5360d2b6
Berzaf <berzafastroo2012@gmail.com>
2021-01-12 20:57:01 -0600

    Express Session Part 2

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function auth(req, res, next) {
       const user = auth[0];
       const pass = auth[1];
       if (user === 'admin' && pass === 'password') {
-          res.session.user = 'admin';
+          req.session.user = 'admin';
           return next(); // authorized
       } else {
           const err = new Error('You are not authenticated!');
