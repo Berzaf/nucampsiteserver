80c24c11fdff0e196be07be5d8b5e19b7359e307
Berzaf <berzafastroo2012@gmail.com>
2021-01-23 09:52:07 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
     'facebook': {
-        clientId: '247101076990404',
-        clientSecret: '35aa4925e6d0b68555b1a614078624a1'
+       // clientId: '247101076990404',
+       // clientSecret: '35aa4925e6d0b68555b1a614078624a1'
     }
 }
